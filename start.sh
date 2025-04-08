5b810c13a159300132d3a227049064d7d7639164
Lightshzk <light31shzk@gmail.com>
2025-04-08 08:57:52 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+while : 
+do
+echo -e "${GREEN} Tomori - Auto reconexão ativada para prevenção de quedas.."
+
+if [ "$1" = "sim" ]; then
+node iniciar.js sim
+elif [ "$1" = "não" ]; then
+node iniciar.js não
+else 
+node iniciar.js
+fi
+
+sleep 1
+done
 
